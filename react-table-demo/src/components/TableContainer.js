0e48b7240adaef49a62c5156269aca81073564f9
Isaac <finken.isaac@gmail.com>
2021-02-03 04:28:42 -0600

    Clean up TableContainer file.

--- react-table-demo/src/components/TableContainer.js
+++ react-table-demo/src/components/TableContainer.js
@@ -1,141 +1,19 @@
 // filtering start https://blog.logrocket.com/building-styling-tables-react-table-v7/#filtering
 // freeze header pane https://codesandbox.io/s/k9n3y82wov
 
-// here we set up table structure, data, and styling
 import React, { useEffect, useState, useMemo } from "react";
 import Table from "./Table";
-// import styled from "styled-components";
 
-// const Styles = styled.div`
-//   padding: 1rem;
-
-//   * {
-//     box-sizing: border-box;
-//     text-align: center;
-//   }
-
-//   .table {
-//     /* border: 1px solid #000; */
-//     /* max-width: 700px; */
-//     overflow-x: auto;
-//     /* overflow-x: hidden; */
-//   }
-
-//   .header {
-//     font-weight: 600;
-//   }
-
-//   .rows {
-//     /* overflow-y: hidden;
-//     overflow-x: hidden; */
-//   }
-
-//   .row {
-//     /* border-bottom: 1px solid #000; */
-//     height: 200px;
-
-//     &.body {
-//       :last-child {
-//         border: 0;
-//       }
-//     }
-//   }
-
-//   .header-group {
-//     height: 40px;
-//     /* 40px; */
-//     background-color: lightgrey;
-//     white-space: nowrap;
-//     display: inline-block;
-//     box-sizing: border-box;
-//     line-height: 30px;
-//     /* border: 1px solid green; */
-//     /* margin: 10px!important; */
-//   }
-
-//   .header-group-search {
-//     height: 80px !important;
-//   }
-
-//   .header.cell {
-//     /* border: 1px solid green; */
-//     height: 100%;
-//     border-top: 5px solid transparent;
-//     border-bottom: 5px solid transparent;
-//     overflow: hidden;
-//   }
-//   .header.cell:focus {
-//     outline: none;
-//   }
-//   .current-dropzone {
-//     opacity: 0.6;
-//   }
-//   .row.body:hover {
-//     background-color: rgb(250, 100, 100) !important;
-//   }
-
-//   .cell {
-//     height: 100%;
-//     /* line-height: 31px; */
-//     border-right: 1px solid #000;
-//     /* padding-left: 5px; */
-//     overflow-x: hidden;
-
-//     :last-child {
-//       border-right: 0;
-//     }
-//   }
-//   .resizer {
-//     display: inline-block;
-//     background: rgba(173, 216, 230, 0.7);
-//     width: 5px;
-//     height: 40px;
-//     position: absolute;
-//     right: 0;
-//     top: 0;
-//     /* transform: translateX(5px); */
-//     z-index: 1;
-//     overflow-x: visible !important;
-//     ${"" /* prevents from scrolling while dragging on touch devices */}
-//     touch-action:none;
-//     transition: width 0.5s;
-
-//     &.isResizing {
-//       background: rgba(173, 216, 230, 1);
-//     }
-//     &:hover {
-//       width: 10px;
-//       background: rgba(173, 216, 230, 1);
-
-//       /* transform: translateX(0); */
-//     }
-//   }
-
-//   /* !!! */
-//   .filter {
-//     /* width: 100%; */
-//     /* width: 100px; */
-//     cursor: default;
-//     /* height: 100%; */
-//     /* border: 4px solid purple; */
-//     box-sizing: border-box;
-//     z-index: 10;
-//     position: absolute;
-//     bottom: 0px;
-//     /* border: 1px solid blue; */
-//   }
-//   /* -!!! */
-// `;
-
+// here we set up table structure, data, and styling
 export default function TableContainer() {
   const [tableData, setTableData] = useState([]);
-	// load initial data.
+  // load initial data.
 
   useEffect(() => {
     async function getData() {
-			let skip = 0;
-			// skip<3000 because too slow otherwise... this is a problem
-      while (skip<3000) {
+      let skip = 0;
+      // skip<3000 because too slow otherwise... this is a problem
+      while (skip < 3000) {
         console.log("skip and table data length: ", skip, tableData.length);
         const res = await fetch(
           `https://api.fda.gov/food/event.json?limit=1000&skip=${skip}`
@@ -159,36 +37,14 @@ export default function TableContainer() {
     // pass in page, NOT rows - rows too much data and no benefit
     const maxWidth = 400;
     const magicSpacing = 10;
-    // console.log(rows);
     const rowLenArr = rows.map((row) => {
-      // console.log(`${row[accessor]}` || "");
       return (`${row[accessor]}` || "").length;
     });
     const potentialLenArr = [...rowLenArr, headerText.length];
     const cellLength = Math.max(...potentialLenArr);
     const result = Math.min(maxWidth, cellLength * magicSpacing);
-    // console.log(
-    //   "accessor:",
-    //   accessor,
-    //   "headerText.length: ",
-    //   headerText.length,
-    //   "cellLength: ",
-    //   cellLength,
-    //   "result: ",
-    //   result
-    // );
     return result;
   };
-
-  // const getColumnWidthOriginal = (rows, accessor, headerText) => {
-  //   const maxWidth = 400;
-  //   const magicSpacing = 10;
-  //   const cellLength = Math.max(
-  //     ...rows.map((row) => (`${row[accessor]}` || "").length),
-  //     headerText.length
-  //   );
-  //   return Math.min(maxWidth, cellLength * magicSpacing);
-  // };
 
   const data = useMemo(() => {
     const flatData = [];
@@ -232,9 +88,6 @@ export default function TableContainer() {
           {
             Header: "Age",
             accessor: "consumer.age",
-            Cell: ({ cell: { value } }) => (
-              <span style={{ color: "red" }}>{value}</span>
-            ),
             width: 40,
             filter: "text",
           },
@@ -316,9 +169,5 @@ export default function TableContainer() {
     []
   );
 
-  return (
-    // <Styles>
-      <Table columns={columns} data={data} />
-    // </Styles>
-  );
+  return <Table columns={columns} data={data} />;
 }
