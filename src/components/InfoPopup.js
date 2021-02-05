5699e1b55fac6f7a9fbc3363b2fd9e02b36660dc
Isaac <finken.isaac@gmail.com>
2021-02-05 13:22:42 -0600

    Added modal to info icon click - need to add contents.

--- src/components/InfoPopup.js
+++ src/components/InfoPopup.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function InfoPopup(){
-	return(
-		<div className='header__info-popup'>
-			&#x1F6C8;
-		</div>
-	)
+export default function InfoPopup() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleClick = () => {
+    console.log("hits");
+    setShowPopup(true);
+  };
+  return (
+    <>
+      <div className="header__info-popup-container">
+        <div className="header__info-popup-button" onClick={handleClick}>
+          &#x1F6C8;
+        </div>
+      </div>
+      <Popup setShowPopup={setShowPopup} showPopup={showPopup} />
+    </>
+  );
+}
+
+function Popup({ showPopup, setShowPopup }) {
+  const handleClick = () => {
+    setShowPopup(false);
+  };
+  return (
+    <div className={`popup ${showPopup ? "show-popup" : ""}`}>
+      <div className="popup-content">
+        <span className="close-button" onClick={handleClick}>
+          &times;
+        </span>
+        <h1>Hello, I am a modal!</h1>
+      </div>
+    </div>
+  );
 }
